c05feb17225cea51fa997ee99364d2f53b1ad612
Cesmac <cesmac@academico.cesmac.edu.br>
2023-08-14 21:24:04 -0300

    aula03

--- altamirblog/altamirblog/urls.py
+++ altamirblog/altamirblog/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from posts.views import listar_posts
+from posts.views import listar_posts, detalhar_post
 from posts.views import categorias_blogs
 
+
 urlpatterns = [
-    path('', listar_posts),
-    path('categorias/', categorias_blogs)
+    path('posts/', listar_posts),
+    path('categorias/', categorias_blogs),
+    path('posts/<int:id>/', detalhar_post, name="post_detail")
 ]
--- altamirblog/posts/views.py
+++ altamirblog/posts/views.py
@@ -2,21 +2,25 @@ from django.shortcuts import render
 
 posts = [
     {
+    "id": 1,
     "titulo": "Titulo 1",
     "subtitulo": "Subtitulo 1",
     "descricao": "Descricao 1"
     },
     {
+    "id": 2, 
     "titulo": "Titulo 2",
     "subtitulo": "Subtitulo 2",
     "descricao": "Descricao 2"
     },
     {
+    "id": 3,   
     "titulo": "Titulo 3",
     "subtitulo": "Subtitulo 3",
     "descricao": "Descricao 3"
     },
     {
+    "id": 4,
     "titulo": "Titulo 4",
     "subtitulo": "Subtitulo 4",
     "descricao": "Descricao 4"
@@ -43,3 +47,9 @@ def categorias_blogs(request):
     }
     return render(request,"categorias.html", contexto)
 
+def detalhar_post(request, id):
+    post = posts[id-1]
+    return render(request, "detalhar_post.html", {
+        'post': post
+
+    })
