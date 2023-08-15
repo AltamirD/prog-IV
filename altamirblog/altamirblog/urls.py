52d6c6c5f319687b15c99198603fa3cfecc1b8c9
Cesmac <cesmac@academico.cesmac.edu.br>
2023-08-15 19:50:46 -0300

    aula04

--- altamirblog/altamirblog/urls.py
+++ altamirblog/altamirblog/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from posts.views import listar_posts, detalhar_post
 from posts.views import categorias_blogs
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('posts/', listar_posts),
     path('categorias/', categorias_blogs),
     path('posts/<int:id>/', detalhar_post, name="post_detail")
-]
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
