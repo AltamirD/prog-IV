211e9b0b64b10b7674241cc1b9c534dc117efc28
Cesmac <cesmac@academico.cesmac.edu.br>
2023-09-11 21:32:25 -0300

    Adicionando banco de dados

--- altamirblog/altamirblog/urls.py
+++ altamirblog/altamirblog/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('posts/', listar_posts),
     path('categorias/', categorias_blogs),
     path('posts/<int:id>/', detalhar_post, name="post_detail")
--- altamirblog/posts/models.py
+++ altamirblog/posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=255)
+    subtitulo = models.CharField(max_length=255)
+    descricao = models.TextField()
+
+    def __str__ (self) -> str:
+        return f'{self.titulo} <-> {self.subtitulo} <-> {self.descricao} '
